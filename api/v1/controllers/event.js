244716955c075b190b98352bb1eecab5efb6f35c
devwojtek <devwojtek99@gmail.com>
2017-07-07 14:56:53 -0400

    update alert

--- api/v1/controllers/event.js
+++ api/v1/controllers/event.js
@@ -5,7 +5,7 @@ var helper = rootRequire('./api/v1/helper');
 module.exports = {
     listAllEvents: function listAllEvents(req, res, next){
     console.log("get data")
-    connect.db.any("SELECT * FROM event ORDER BY created_at DESC")
+    connect.db.any("SELECT * FROM alerts ORDER BY created_at DESC")
     .then(function(data) {
         return res.status(200)
             .json({
@@ -29,15 +29,15 @@ module.exports = {
         var _params = req.params
             ,id = _params.id ? _params.id : null;
 
-        connect.db.any('SELECT * FROM event WHERE _id = $1', id)
+        connect.db.any('SELECT * FROM alerts WHERE _id = $1', id)
             .then(function(event) {
                 if (event.length > 0) {
-                    event = event[0];
+                    alerts = event[0];
                 } else {
-                    event = null
+                    alerts = null
                 }
 
-                if (event && event != null) {
+                if (event && alerts != null) {
                     return res.status(200)
                         .json({
                             status: 'success',
@@ -72,23 +72,23 @@ module.exports = {
         if (!_params.id) {
             return res.send({ status: 0, message: 'Invalid parameters' });
         }
-        connect.db.any('SELECT * FROM event WHERE id = $1', _params.id)
+        connect.db.any('SELECT * FROM alerts WHERE id = $1', _params.id)
             .then(function(event) {
                 if (event.length > 0) {
-                    event = event[0];
+                    alerts = event[0];
                 } else {
-                    event = null
+                    alerts = null
                 }
-                if (event && event != null) {
+                if (event && alerts != null) {
                     _body.title = _body.title ? _body.title : event.title;
 
-                    connect.db.one('update event set title=$1 where id = $2 RETURNING * ', [_body.title, _params.id])
+                    connect.db.one('update alerts set title=$1 where id = $2 RETURNING * ', [_body.title, _params.id])
                         .then(function(data) {
                             res.status(200)
                                 .json({
                                     status: 'success',
                                     data: data,
-                                    message: 'Updated event successfully.'
+                                    message: 'Updated alerts successfully.'
                                 });
                         })
                         .catch(function(err) {
@@ -131,7 +131,7 @@ module.exports = {
             return res.send({ status: 0, message: 'Invalid parameters' });
         }
         // delete;
-        connect.db.result('DELETE FROM event WHERE id = $1', _params.id)
+        connect.db.result('DELETE FROM alerts WHERE id = $1', _params.id)
             .then(function(result) {
                 // rowCount = number of rows affected by the query
                 if (result.rowCount > 0) {
@@ -172,7 +172,7 @@ module.exports = {
             return res.send({ status: 0, message: 'Invalid parameters' });
         }
 
-        connect.db.one('INSERT INTO event (title, body, owner, created_at ) VALUES($1, $2, $3, $4) RETURNING *', [_body.title, _body.body, _body.owner, _body.created_at])
+        connect.db.one('INSERT INTO alerts (title, body, owner, created_at ) VALUES($1, $2, $3, $4) RETURNING *', [_body.title, _body.body, _body.owner, _body.created_at])
         .then(function(data) {
             return res.status(200)
                 .json({
@@ -200,7 +200,7 @@ module.exports = {
 
         console.log("message = ", message)
 
-        connect.db.one('INSERT INTO event (title, body, owner, created_at ) VALUES($1, $2, $3, $4) RETURNING *', [title, body, owner, created_at])
+        connect.db.one('INSERT INTO alerts (title, body, owner, created_at ) VALUES($1, $2, $3, $4) RETURNING *', [title, body, owner, created_at])
             .then(function(data) {
                 console.log("inserted successfully ", data)
               io.emit('broad-event', {message: body, title:title, created_at: created_at });
